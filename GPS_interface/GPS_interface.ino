bc9aa67120ebbc1c1cac39fae748b3faf296bcc1
jamesboby897 <78583580+jamesboby897@users.noreply.github.com>
2021-09-04 22:25:45 +0530

    Bluetooth implemented
    Bluetooth serial communication implemented

--- GPS_interface/GPS_interface.ino
+++ GPS_interface/GPS_interface.ino
@@ -1,9 +1,11 @@
+#include <BluetoothSerial.h>
 #include <HardwareSerial.h>
 HardwareSerial GPS_Module(1);
+BluetoothSerial BtSerial;
 void setup() 
 {
   GPS_Module.begin(9600,SERIAL_8N1,16,17);
-  Serial.begin(115200);
+  BtSerial.begin();
 }
 
 void loop() 
@@ -11,6 +13,6 @@ void loop()
   while(GPS_Module.available() > 0)
   {
     uint8_t bytes = GPS_Module.read();
-    Serial.write(bytes);
+    BtSerial.write(bytes);
   }
 }
